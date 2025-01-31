cb0699511dcad637f44dd0e27e73b415ae6de3f6
Gor44 <gordonzhang2017@gmail.com>
2025-01-31 16:39:58 -0500

    review

--- webapp/moviestore/forms.py
+++ webapp/moviestore/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['movie_title', 'review_text', 'rating']
 
 class LoginForm(forms.Form):
     username = forms.CharField()

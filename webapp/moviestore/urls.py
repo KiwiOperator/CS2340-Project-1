f96715a51ab6e1bfdd0e71470cca009d73205838
KiwiOperator <billgao503@gmail.com>
2025-01-31 17:08:18 -0500

    Fix merge conflicts

--- webapp/moviestore/urls.py
+++ webapp/moviestore/urls.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import login_view, signup_view, success_view
+from .views import login_view, signup_view, success_view, create_review, edit_review, delete_review
 
 urlpatterns = [
     path("login/", login_view, name="login"),
     path("signup/", signup_view, name="signup"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("success/", success_view, name="success"),
-    path('review/create/', views.create_review, name="create_review"),
-    path('review/edit/<int:review_id>/', views.edit_review, name="edit_review"),
-    path('review/delete/<int:review_id>/', views.delete_review, name="delete_review"),
+    path('review/create/', create_review, name="create_review"),
+    path('review/edit/<int:review_id>/', edit_review, name="edit_review"),
+    path('review/delete/<int:review_id>/', delete_review, name="delete_review"),
 ]

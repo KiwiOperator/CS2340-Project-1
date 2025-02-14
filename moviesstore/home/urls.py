18608d53ec5f95c72c98893e516a279f435616e2
ypan666 <ypan414@gatech.edu>
2025-02-13 19:31:27 -0500

    updated home page and show total price + remove single movies in cart app

--- moviesstore/home/urls.py
+++ moviesstore/home/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import home_view
-
 urlpatterns = [
     path('', home_view, name='home'),  # Homepage must point to home_view
     path('', views.index, name='home.index'),
